86d67a4e396ae1b2b5ae724f1a3968248b5df8a7
gitofdhananjay <85330158+gitofdhananjay@users.noreply.github.com>
2021-10-16 20:07:50 +0530

    Create a simple String calculator with a method signature: ——————————————— int Add(string numbers) ——————————————— The method can take up to two numbers, separated by commas, and will return their sum. for example “” or “1” or “1,2” as inputs. (for an empty string it will return 0) Hints: ——————  - Start with the simplest test case of an empty string and move to one and two numbers  - Remember to solve things as simply as possible so that you force yourself to write tests you did not think about  - Remember to refactor after each passing test ——————————————————————————————— Allow the Add method to handle an unknown amount of numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,24 @@
 package calculator;
 
+import custom_exception.StringCalculaterException;
+
 class StringCalculator {
+	String delimeter = ",";
 
     public int add(String input) {
-        return 0;
+    	if(input.isEmpty())
+    		return 0;
+    	
+    	return sum(input);
+    	
+    		
     }
-
+    private int sum(String input) {	
+		String[] srr = input.split(",");
+		int sum = 0;
+		for(String s : srr) {
+			sum += Integer.parseInt(s);
+		}
+		return sum;
+	}
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,20 +1,27 @@
 package calculator;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
 
-class StringCalculatorShould {
+public class StringCalculatorShould {
+	
+	private StringCalculator sCal;
+	
+	@Before
+	public void setup () {
+		sCal = new StringCalculator();
+	}
 
     @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+    public void empty_string_should_return_0() {
+        assertEquals(0, sCal.add(""));
     }
 
     @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+    public void string_with_single_number_should_return_number_as_int() {
+        assertEquals(1, sCal.add("1"));
+        assertEquals(12, sCal.add("1,5,6"));
     }
 }
